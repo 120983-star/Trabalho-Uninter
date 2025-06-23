e3e08057a0629dbc98e95283a44b1751834ab8ce
120983-star <zaqueuralves@hotmail.com>
2025-06-22 22:08:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const paragrafoMensagem = document.getElementById('mensagem');
 
     botaoMensagem.addEventListener('click', function() {
-        // A linha abaixo foi alterada para a mensagem religiosa
-        paragrafoMensagem.textContent = 'Que Deus te abençoe!';
+        // A linha abaixo foi atualizada com a mensagem mais elaborada.
+        paragrafoMensagem.textContent = 'Seja bem-vindo(a) a este espaço! Que a sua visita seja abençoada e que você encontre inspiração e paz. Deus guie sempre os seus passos.';
     });
 });
